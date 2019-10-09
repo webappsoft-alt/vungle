4c39be1f8b6e15eb43b6081d49bae6211691e51c
webappsoft-alt <56319619+webappsoft-alt@users.noreply.github.com>
2019-10-09 04:52:28 -0700

    Update vungle.java

--- src/android/vungle.java
+++ src/android/vungle.java
@@ -6,7 +6,7 @@ import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
